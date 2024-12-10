372eea3c2647267ce9428da22c449b1713445218
immber <jessiekrushing@gmail.com>
2024-12-10 13:49:28 -0700

    added readme and npm cmds

---
+++ README.md
@@ -0,0 +1,22 @@
+# jpt-product-pg-gen
+
+Tool to make /product pages on JustPolyThings.com
+
+## Requirements
+
+### CSV input
+
+see `/sample.csv` for example layout
+
+### Env Vars
+see `/.sample-env` for sample .env file 
+
+
+### NPM Commands
+
+* `npm test` executes script.js with `/sample.csv` as input
+* `npm start` executes script.js with `/live.csv` as input
+
+## Usage
+
+Update `/live.csv` and `/src/definitions.js` and run with `npm start`!
--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-import { readFile } from 'node:fs';
+import fs from 'fs';
+import { parse } from "csv-parse";
 
 import {AMAZON_KEY} from './src/definitions.js'
 
 const records = [];
 const csvFile = process.argv[2];
 
-if (csvFile) {
-    readFile(csvFile, (err, data) => {
-        if (err) ;
-       console.log(data);
-    });
-} else {
-    
+if (!csvFile) {
     throw new Error('missing csv file');
 }
 
+fs.createReadStream(csvFile, (err) => {
+    if (err) ;
+    }).pipe(parse({ delimiter:",", from_line: 2 })
+    .on("data", function(row){
+        records.push(row);
+    })
+    .on("end", function(){
+        console.log(records);
+    }))
+
+    
 console.log(AMAZON_KEY);
+
+
 
 
 
